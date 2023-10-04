10c9a825b4f9d65e7df14fae33e2eea3314d3a77
vidhit1084 <vidhit@metadome.ai>
2023-10-04 17:00:52 +0530

    updated

--- main.js
+++ main.js
@@ -1,41 +1,41 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
-const fs = require("fs");
+const fs = require("fs/promises"); // Using 'fs/promises' for async file operations
 const path = require("path");
-const app = express();
-const { exec } = require("child_process");
+const { exec, spawn } = require("child_process"); // Import spawn from child_process
 
+const app = express();
 const port = 3001;
 let check = false;
-let track = false;
-// Serve static files (HTML, CSS, etc.) from the public directory
-app.use(express.static("public"));
+let flag = false;
+let jsonData;
 
-// Parse application/json
+const publicDir = path.join(process.cwd(), "public");
+
+app.use(express.static(publicDir));
 app.use(bodyParser.json());
 
-//Function to fetch json data
 const fetchData = async () => {
-  const dataDir = path.join(__dirname, "heartbeatData");
-  if (!fs.existsSync(dataDir)) {
-    fs.mkdirSync(dataDir);
-  }
-
-  const jsonFiles = fs
-    .readdirSync(dataDir)
-    .filter((file) => file.endsWith(".json"));
-  if (jsonFiles.length > 0) {
-    const firstJsonFile = jsonFiles[0];
-    let jsonData = JSON.parse(
-      fs.readFileSync(path.join(dataDir, firstJsonFile))
+  const dataDir = path.join(process.cwd(), "data");
+  try {
+    await fs.mkdir(dataDir, { recursive: true }); // Ensure the directory exists
+    const jsonFiles = (await fs.readdir(dataDir)).filter((file) =>
+      file.endsWith(".json")
     );
-    return jsonData;
+    if (jsonFiles.length > 0) {
+      const firstJsonFile = jsonFiles[0];
+      const jsonData = JSON.parse(
+        await fs.readFile(path.join(dataDir, firstJsonFile), "utf-8")
+      );
+      return jsonData;
+    }
+  } catch (error) {
+    console.error("Error reading JSON data:", error);
   }
   return null;
 };
 
-//Function to check if app is running
 const checkAppRunning = async (appName) => {
   if (!appName) return false;
   return new Promise((resolve, reject) => {
@@ -44,7 +44,6 @@ const checkAppRunning = async (appName) => {
         if (!error && stdout) {
           resolve({ success: true, isRunning: true });
         } else {
-          // resolve({ success: false, isRunning: false });
           console.log(`app ${appName} isn't running`);
           reject(new Error(`app ${appName} isn't running`));
           console.log("failed");
@@ -74,11 +73,11 @@ const initializeJsonData = async () => {
     console.log(jsonData);
   }
 };
-// Function to send post request via url
+
 const sendPing = async (jsonData) => {
   try {
     const apiResponse = await axios.post(
-      "http://localhost:3000/ping",
+      "https://api.metadome.ai/heartbeat-dev/ping",
       jsonData
     );
     console.log("API response:", apiResponse.data);
@@ -98,111 +97,74 @@ app.post("/submit", async (req, res) => {
     return res.status(400).send("Missing client, store, or software.");
   }
 
-  // Store the data locally in a JSON file
-
-  const filePath = path.join(
-    __dirname,
-    "heartbeatData",
-    `${formData.client}.json`
-  );
+  const filePath = path.join(process.cwd(), "data", `${formData.client}.json`);
 
   const jsonData = JSON.stringify(formData, null, 2);
-  fs.writeFileSync(filePath, jsonData);
-
-  initializeJsonData();
-  const appsArray = formData.appsArray || [];
-  console.log(appsArray);
-  flag = true;
-  return res.redirect("/success");
-  //   for (let i = 0; i < appsArray.length; i++) {
-  //     let eachApp = appsArray[i];
-  //     console.log(eachApp, "that's it");
-
-  //     try {
-  //       const checkApp = await checkAppRunning(eachApp);
-  //       if (checkApp) {
-  //         console.log(`app ${eachApp} is running`);
-  //         const appData = {
-  //           client: formData.client,
-  //           store: formData.store,
-  //           software: formData.software,
-  //           app: eachApp,
-  //         };
-  //         console.log(appData, "this is app data");
-
-  //         await sendPing(appData); // Don't handle response here, let it propagate up
-  //       } else {
-  //         console.log("App is not running");
-  //       }
-  //     } catch (error) {
-  //       console.error("Error checking app status:", error.message);
-  //       return res.status(500).send("Error checking app status.");
-  //     }
-  //   }
-
-  // Send a successful response after all iterations are complete
-  //   return res.redirect("success.html");
+  try {
+    await fs.writeFile(filePath, jsonData);
+    initializeJsonData();
+    const appsArray = formData.appsArray || [];
+    console.log(appsArray);
+    flag = true;
+    return res.redirect("/success");
+  } catch (error) {
+    console.error("Error writing JSON file:", error);
+    return res.status(500).send("Error writing JSON file.");
+  }
 });
 
-// app.use((req, res, next) => {
-//   const currentPath = req.path;
-//   if (currentPath === "/success") {
-//     flag = true;
-//   } else if (currentPath === "/" || currentPath === "/home") {
-//     flag = false;
-//   }
-//   console.log(`Flag updated to: ${flag}`);
-
-//   next();
-// });
-
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(process.cwd(), "public", "index.html"));
 });
 
 app.get("/home", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "data.html"));
+  res.sendFile(path.join(process.cwd(), "public", "data.html"));
 });
 
 app.get("/success", (req, res) => {
   console.log("hi");
   flag = true;
-  res.sendFile(path.join(__dirname, "public", "success.html"));
+  res.sendFile(path.join(process.cwd(), "public", "success.html"));
 });
 
 app.post("/flag", (req, res) => {
   let { flag } = req.body;
 
-  // Update the flag variable with the received value
   if (flag !== undefined) {
-    // Convert the value to a boolean
     const newFlag = Boolean(flag);
-
-    // Update the global flag variable
     flag = newFlag;
     check = flag;
     console.log(check);
-    res.json({ flag: newFlag }); // Send back the updated flag value
+    res.json({ flag: newFlag });
   } else {
     res.status(400).json({ error: "Invalid flag value" });
   }
 });
 
-let jsonData;
 app.listen(port, async () => {
   console.log(`Server is running on port ${port}`);
-  const open = await import("open");
-
-  // Open the HTML page in the default web browser
   jsonData = await fetchData();
   console.log(jsonData, "this is json data");
   flag = false;
   if (jsonData) {
-    open.default(`http://localhost:${port}/home`);
+    openBrowser(`http://localhost:${port}/home`);
   } else {
-    open.default(`http://localhost:${port}`);
+    openBrowser(`http://localhost:${port}`);
   }
 });
+
+const openBrowser = (url) => {
+  switch (process.platform) {
+    case "darwin":
+      spawn("open", [url]);
+      break;
+    case "win32":
+      spawn("start", [url], { shell: true });
+      break;
+    default:
+      spawn("xdg-open", [url]);
+  }
+};
 
 const main = async () => {
   console.log(check);
@@ -213,7 +175,7 @@ const main = async () => {
       console.log("data not found");
     } else {
       console.log("Checking app status of :", jsonData);
-      const appsArray = jsonData.appsArray || []; // Ensure appsArray is an array
+      const appsArray = jsonData.appsArray || [];
       console.log(appsArray, "appsArray");
       for (let i = 0; i < appsArray.length; i++) {
         let eachApp = appsArray[i];
@@ -224,20 +186,19 @@ const main = async () => {
           if (checkApp) {
             console.log(`app ${eachApp} is running`);
             const appData = {
-              client: jsonData.client, // Use jsonData.client
-              store: jsonData.store, // Use jsonData.store
-              software: jsonData.software, // Use jsonData.software
+              client: jsonData.client,
+              store: jsonData.store,
+              software: jsonData.software,
               app: eachApp,
             };
             console.log(appData, "this is app data");
 
-            await sendPing(appData); // Don't handle response here, let it propagate up
+            await sendPing(appData);
           } else {
             console.log("App is not running");
           }
         } catch (error) {
           console.error("Error checking app status:", error.message);
-          // You might want to handle errors differently here
         }
       }
     }
